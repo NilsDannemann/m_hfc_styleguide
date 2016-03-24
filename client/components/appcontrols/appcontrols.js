21d07098bd84eb29f2279231379364e7670c92ed
Nils Dannemann <ndannemann@gmail.com>
2016-03-24 17:13:41 +0100

    quick fixes

--- client/components/appcontrols/appcontrols.js
+++ client/components/appcontrols/appcontrols.js
@@ -1,5 +1,5 @@
 Template.appcontrols.events({
-	'click .icon-equalizer': function(){
+	'click .js-toggle-filter': function(){
 		var app = Session.get('app');
 		app.content.asideR.toggled = !app.content.asideR.toggled;
 		app.footer.asideR.toggled = !app.footer.asideR.toggled;
