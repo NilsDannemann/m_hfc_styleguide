7354d14a7c65982b46c1f909edf290523bed23d7
Nils Dannemann <ndannemann@gmail.com>
2016-04-01 07:40:37 +0200

    [removed] is-expanded state

--- client/layouts/app/app.js
+++ client/layouts/app/app.js
@@ -1,28 +1,22 @@
 // Define initial app state
-var props = function() {
-	return {
-		toggled: false,
-		expanded: false
-	};
-};
 var app = {
 	header: {
-		asideL: props(),
-		main: props(),
-		asideR: props()
+		asideL: {toggled: false},
+		main: 	{toggled: false},
+		asideR: {toggled: false}
 	},
 	content: {
-		asideL: props(),
-		main: props(),
-		asideR: props()
+		asideL: {toggled: false},
+		main: 	{toggled: false},
+		asideR: {toggled: true}
 	},
 	footer: {
-		asideL: props(),
-		main: props(),
-		asideR: props()
+		asideL: {toggled: false},
+		main: 	{toggled: false},
+		asideR: {toggled: true}
 	},
-	drawerL: props(),
-	drawerR: props()
+	drawerL: 	{toggled: false},
+	drawerR: 	{toggled: false}
 };
 
 
@@ -37,15 +31,12 @@ Template.app.helpers({
 	// Header
 	headerAsideLState: function(){
 		if (Session.get('app').header.asideL.toggled) return 'is-toggled';
-		if (Session.get('app').header.asideL.expanded) return 'is-expanded';
 	},
 	headerMainState: function(){
 		if (Session.get('app').header.main.toggled) return 'is-toggled';
-		if (Session.get('app').header.main.expanded) return 'is-expanded';
 	},
 	headerAsideRState: function(){
 		if (Session.get('app').header.asideR.toggled) return 'is-toggled';
-		if (Session.get('app').header.asideR.expanded) return 'is-expanded';
 	},
 	// Content
 	contentAsideLState: function(){
@@ -57,15 +48,12 @@ Template.app.helpers({
 	// Footer
 	footerAsideLState: function(){
 		if (Session.get('app').footer.asideL.toggled) return 'is-toggled';
-		if (Session.get('app').footer.asideL.expanded) return 'is-expanded';
 	},
 	footerMainState: function(){
 		if (Session.get('app').footer.main.toggled) return 'is-toggled';
-		if (Session.get('app').footer.main.expanded) return 'is-expanded';
 	},
 	footerAsideRState: function(){
 		if (Session.get('app').footer.asideR.toggled) return 'is-toggled';
-		if (Session.get('app').footer.asideR.expanded) return 'is-expanded';
 	},
 	// Drawer
 	DrawerLState: function(){
@@ -100,37 +88,6 @@ Template.app.events({
 	'click .js-toggle--drawer-r': function(){
 		var app = Session.get('app');
 		app.drawerR.toggled = !app.drawerR.toggled;
-		Session.set('app', app);
-	},
-	// Expand
-	'click .app__header-aside-l': function(){
-		var app = Session.get('app');
-		app.header.asideL.expanded = !app.header.asideL.expanded;
-		Session.set('app', app);
-	},
-	'click .app__header-main': function(){
-		var app = Session.get('app');
-		app.header.main.expanded = !app.header.main.expanded;
-		Session.set('app', app);
-	},
-	'click .app__header-aside-r': function(){
-		var app = Session.get('app');
-		app.header.asideR.expanded = !app.header.asideR.expanded;
-		Session.set('app', app);
-	},
-	'click .app__footer-aside-l': function(){
-		var app = Session.get('app');
-		app.footer.asideL.expanded = !app.footer.asideL.expanded;
-		Session.set('app', app);
-	},
-	'click .app__footer-main': function(){
-		var app = Session.get('app');
-		app.footer.main.expanded = !app.footer.main.expanded;
-		Session.set('app', app);
-	},
-	'click .app__footer-aside-r': function(){
-		var app = Session.get('app');
-		app.footer.asideR.expanded = !app.footer.asideR.expanded;
 		Session.set('app', app);
 	}
 });
